741655de749586a00d2dc60f0b979ed7c7e3e706
tosei.hatori <tosei.hatori@gmail.com>
2024-07-26 09:39:04 +0900

    update

---
+++ run/conf/exp/exp_24_convnext_small_baseline_5fold_w01.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# GPU番号を引数として受け取る
+gpu_index=$1
+
+SCRIPT_NAME=$(basename "$0")
+
+for FOLD in {0..4}; do
+  CUDA_VISIBLE_DEVICES=$gpu_index python -m run.train \
+    dataset.num_folds=4 \
+    dataset.fold_path=./fold/train_with_fold_5.csv \
+    dataset.test_fold=$FOLD \
+    dataset.use_cache=false \
+    dataset.downsampling_rate=0 \
+    dataset.downsampling_by_patient=false \
+    training.batch_size=16 \
+    training.batch_size_test=32 \
+    training.epoch=5 \
+    preprocessing.h_resize_to=256 \
+    preprocessing.w_resize_to=256 \
+    augmentation.use_light_aug=true \
+    augmentation.p_coarse_dropout=0.5 \
+    augmentation.coarse_dropout_max_holes=16 \
+    augmentation.coarse_dropout_max_size=32 \
+    model.base_model=convnext_small.fb_in22k_ft_in1k \
+    training.use_wandb=true \
+    training.num_workers=24 \
+    forwarder.loss.has_lesion_id_weight=0.1 \
+    optimizer.lr=2e-5 \
+    optimizer.lr_head=2e-4 \
+    scheduler.warmup_steps_ratio=0.0 \
+    training.accumulate_grad_batches=2 \
+    out_dir=../results/${SCRIPT_NAME}_${FOLD}
+
+done
--- src/utils/get_fold.py
+++ src/utils/get_fold.py
@@ -1,17 +1,34 @@
 import pandas as pd
-from sklearn.model_selection import GroupKFold
+from sklearn.model_selection import StratifiedGroupKFold
+import logging
+
+# ロガーの設定
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # データの読み込み
 train_df = pd.read_csv("./data/train-metadata.csv", low_memory=False)
 
-# Group K-Foldで分割
-n_splits = 4
-gkf = GroupKFold(n_splits=n_splits)
+# Stratified Group K-Foldで分割
+n_splits = 5
+sgkf = StratifiedGroupKFold(n_splits=n_splits)
 train_df["fold"] = -1
 
 for fold, (train_idx, val_idx) in enumerate(
-    gkf.split(train_df, groups=train_df["patient_id"])
+    sgkf.split(train_df, train_df["target"], groups=train_df["patient_id"])
 ):
     train_df.loc[val_idx, "fold"] = fold
+    
+    # foldごとの件数や目的変数の件数をロギング
+    fold_size = len(val_idx)
+    target_counts = train_df.loc[val_idx, "target"].value_counts().to_dict()
+    
+    logger.info(f"Fold {fold}")
+    logger.info(f"Total samples in fold: {fold_size}")
+    logger.info(f"Target counts in fold: {target_counts}")
 
-train_df.to_csv("./fold/train_with_fold_4.csv", index=False)
+# fold = -1 が残っていないか確認
+assert -1 not in train_df["fold"].values, "There are samples with fold = -1"
+
+# 結果を保存
+train_df.to_csv(f"./fold/train_with_fold_{n_splits}.csv", index=False)
